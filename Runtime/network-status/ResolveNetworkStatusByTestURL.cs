017af645673034077b3782b964a267919a628d8b
Larry Kirschner <larrykirschner@gmail.com>
2018-11-28 17:46:43 -0800

    added async support

--- Runtime/network-status/ResolveNetworkStatusByTestURL.cs
+++ Runtime/network-status/ResolveNetworkStatusByTestURL.cs
@@ -1,10 +1,13 @@
 using System;
-using BeatThat.Bindings;
 using BeatThat.DependencyInjection;
 using BeatThat.Requests;
 using BeatThat.Service;
 using BeatThat.StateStores;
 using UnityEngine;
+
+#if NET_4_6
+using System.Threading.Tasks;
+#endif
 
 namespace BeatThat.NetworkStatus
 {
@@ -12,40 +15,90 @@ namespace BeatThat.NetworkStatus
         proxyInterfaces: new Type[] { typeof(StateResolver<NetworkStatusData>) },
         priority: int.MaxValue // so will be overridden by anything with a lower priority
     )]
-    public class ResolveNetworkStatusByTestURL : BindingService, StateResolver<NetworkStatusData>
+    public class ResolveNetworkStatusByTestURL : DefaultStateResolver<NetworkStatusData>
     {
         virtual protected string NextTestUrl()
         {
             return "https://www.google.com";
         }
 
-        public Request<ResolveResponseDTO<NetworkStatusData>> Resolve(Action<Request<ResolveResponseDTO<NetworkStatusData>>> callback)
+#if NET_4_6
+        public async Task<ResolveResultDTO<NetworkStatusData>> ResolveAsync(
+            Action<Request<ResolveResultDTO<NetworkStatusData>>> callback
+        )
         {
-            var promise = new Promise<ResolveResponseDTO<NetworkStatusData>>((resolve, reject) =>
+            var nstatus = new NetworkStatusData
             {
-                new WebRequest(NextTestUrl()).Execute(result =>
+                networkReachability = Application.internetReachability
+            };
+
+            if(nstatus.networkReachability == NetworkReachability.NotReachable) {
+                return ResolveResultDTO<NetworkStatusData>.ResolveSucceeded(nstatus);
+            }
+
+            var req = new WebRequest(NextTestUrl());
+
+            await req.ExecuteAsyncTask();
+
+            var state = new NetworkStatusData();
+
+            state.hasNetworkError = req.www.isNetworkError;
+
+            if(state.hasNetworkError) {
+                state.lastNetworkError = DateTime.Now;
+            }
+            else {
+                state.lastNetworkSuccess = DateTime.Now;
+            }
+
+            return ResolveResultDTO<NetworkStatusData>.ResolveSucceeded(nstatus);
+        }
+#else
+        override public Request<ResolveResultDTO<NetworkStatusData>> Resolve(
+            Action<Request<ResolveResultDTO<NetworkStatusData>>> callback = null)
+        {
+            var nstatus = new NetworkStatusData
+            {
+                networkReachability = Application.internetReachability
+            };
+
+            if (nstatus.networkReachability == NetworkReachability.NotReachable)
+            {
+                var res = new LocalRequest<ResolveResultDTO<NetworkStatusData>>(
+                    ResolveResultDTO<NetworkStatusData>.ResolveSucceeded(nstatus)
+                );
+                res.Execute(callback);
+                return res;
+            }
+
+            var promise = new Promise<ResolveResultDTO<NetworkStatusData>>((resolve, reject) =>
+            {
+                new WebRequest(NextTestUrl()).Execute(req =>
                 {
-                    var state = this.networkStatus.stateData;
+                    var state = new NetworkStatusData();
 
-                    state.hasNetworkError = (result as WebRequest).www.isNetworkError;
+                    state.hasNetworkError = (req as WebRequest).www.isNetworkError;
 
-                    if(state.hasNetworkError) {
+                    if (state.hasNetworkError)
+                    {
                         state.lastNetworkError = DateTime.Now;
                     }
-                    else {
+                    else
+                    {
                         state.lastNetworkSuccess = DateTime.Now;
                     }
 
-                    resolve(new ResolveResponseDTO<NetworkStatusData>
-                    {
-                        status = Constants.STATUS_OK,
-                        data = state
-                    });
+                    resolve(
+                        ResolveResultDTO<NetworkStatusData>.ResolveSucceeded(nstatus)
+                    );
                 });
             });
+
             promise.Execute(callback);
-            return promise;
+
+            return promise;   
         }
+#endif
 
         [Inject] HasState<NetworkStatusData> networkStatus;
     }
